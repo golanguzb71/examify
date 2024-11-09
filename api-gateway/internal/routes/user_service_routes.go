79fe604a8a44e84a0eba4e25cdb6b050cc230268
abdulazizOmonov <abdulaziomonov4@gmail.com>
2024-11-09 12:00:21 +0500

    my-result-role-open

--- api-gateway/internal/routes/user_service_routes.go
+++ api-gateway/internal/routes/user_service_routes.go
@@ -11,5 +11,5 @@ func UserServiceRoutes(r *gin.RouterGroup, authClient *client.AuthClient) {
 	user := r.Group("/user")
 	user.GET("/profile", middleware.AuthMiddleware([]string{"ADMIN", "USER"}, authClient), handlers.GetUserProfile)
 	user.PUT("/update-information", middleware.AuthMiddleware([]string{"USER"}, authClient), handlers.ChangeNameSurname)
-	user.GET("/get-my-results", middleware.AuthMiddleware([]string{"USER"}, authClient), handlers.GetMyResults)
+	user.GET("/get-my-results", middleware.AuthMiddleware([]string{"USER", "ADMIN"}, authClient), handlers.GetMyResults)
 }
