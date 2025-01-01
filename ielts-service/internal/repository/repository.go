782e7a32f6f0497c9e1f65eaa449e8b1128830c2
golanguzb71 <abdulazizomonov4@gmail.com>
2025-01-01 17:29:33 +0500

    monthly_case_edited

--- ielts-service/internal/repository/repository.go
+++ ielts-service/internal/repository/repository.go
@@ -295,7 +295,7 @@ func (r *PostgresRepository) GetTopExamResults(dataframe string, page, size int3
 	var timeframeCondition string
 	switch dataframe {
 	case "MONTHLY":
-		timeframeCondition = `e.created_at >= date_trunc('quarter', CURRENT_DATE)`
+		timeframeCondition = `e.created_at`
 	case "DAILY":
 		timeframeCondition = `e.created_at >= CURRENT_DATE`
 	case "WEEKLY":
